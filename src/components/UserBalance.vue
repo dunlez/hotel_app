06426c2bd29981921ca61dc2babebca0adaacd68
DESKTOP-3FJQRQS\Asus <dunlez10@gmail.com>
2020-12-13 00:33:53 -0500

    modifico link heroku

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -22,7 +22,7 @@ export default {
     this.username = this.$route.params.username;
     let self = this;
     axios
-      .get("https://cajero-api-dunlez.herokuapp.com/user/balance/" + this.username)
+      .get("https://hotel-api-dunlez.herokuapp.com/user/balance/" + this.username)
       .then((result) => {
         self.balance = result.data.balance;
       })
